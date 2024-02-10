94bbc08ec5ba7be6d47fd57aac0a32731c67173d
calmwaves <liux@gitmail.com>
2024-02-11 00:14:11 +0800

    基本实现所需功能

--- main.ts
+++ main.ts
@@ -1,134 +1,148 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {
+	TFile,
+	WorkspaceLeaf,
+	TextFileView,
+	MarkdownView,
+	Plugin,
+} from "obsidian";
 
 interface MyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: "default",
+};
+// todo abstract class ConvertableFileView extends MarkdownView {
+abstract class ConvertableFileView extends TextFileView {
+	plugin: MyPlugin;
+	fileContent: string;
+	header: HTMLElement | null = null;
+	content: HTMLElement | null = null;
+
+	constructor(leaf: WorkspaceLeaf, plugin: MyPlugin) {
+		super(leaf);
+		this.plugin = plugin;
+	}
+
+	getDisplayText(): string {
+		return this.file?.name ?? "???";
+	}
+
+	getContext(file?: TFile) {
+		return file?.path ?? this.file?.path ?? "";
+	}
+
+	async onOpen() {
+		await super.onOpen();
+
+		this.header = document.createElement("div");
+		this.header.id = "docxer-header";
+
+		const warningText = document.createElement("span");
+		warningText.innerText =
+			"Obsidian不支持预览当前文件，点击使用默认应用打开";
+		warningText.style.textAlign = "center";
+		warningText.style.fontSize = "24px";
+		warningText.style.fontWeight = "bold";
+		warningText.style.color = "red";
+		this.header.appendChild(warningText);
+
+		const convertButton = document.createElement("button");
+		convertButton.id = "open-in-default-editor-button";
+		convertButton.innerText = "open in default";
+		//@ts-ignore
+		convertButton.onclick = () => this.app.commands.executeCommandById("open-with-default-app:open");
+		this.header.appendChild(convertButton);
+
+		//todo 勉强可以用，但是用完之后就不刷新侧边栏的反链面板了
+		// const backlinkpane=document.querySelector('.backlink-pane')
+		// if(backlinkpane){this.header.appendChild(backlinkpane);}else{console.log('backlink-pane not found')}
+		this.containerEl.insertAfter(this.header, this.containerEl.firstChild);
+	}
+
+	async onClose() {
+		await super.onClose();
+		if (this.header) this.header.remove();
+	}
+
+	abstract onFileOpen(): Promise<HTMLElement | null>;
+	async onLoadFile(file: TFile) {
+		await super.onLoadFile(file);
+
+		this.content = await this.onFileOpen();
+		if (this.content) this.contentEl.appendChild(this.content);
+	}
+
+	async onUnloadFile(file: TFile) {
+		await super.onUnloadFile(file);
+		if (this.content) this.content.remove();
+	}
+
+	clear(): void {}
+
+	setViewData(data: string): void {
+		this.fileContent = data;
+	}
+
+	getViewData(): string {
+		return this.fileContent;
+	}
+}
+class WarningView extends ConvertableFileView {
+	static readonly VIEW_TYPE = "warning-view";
+
+	getViewType(): string {
+		return WarningView.VIEW_TYPE;
+	}
+
+	async onFileOpen(): Promise<HTMLElement | null> {
+		return null;
+	}
 }
 
+const FILETYPE_MAP: { [key: string]: typeof ConvertableFileView } = {
+	"7z": WarningView,
+	pptx: WarningView,
+	docx: WarningView,
+	xlsx: WarningView,
+	html: WarningView,
+	zip: WarningView,
+	rtf: WarningView,
+	epub: WarningView,
+	mobi: WarningView,
+};
+
+function registerFilePreviews(plugin: Plugin) {
+	// @ts-ignore
+	plugin.registerView(WarningView.VIEW_TYPE,(leaf) => new WarningView(leaf, plugin));
+	for (const [filetype, view] of Object.entries(FILETYPE_MAP)) {
+		// @ts-ignore
+		plugin.registerExtensions([filetype], view.VIEW_TYPE);
+		// console.log(`registering extension for ${filetype}`);
+	}
+}
+
+//!main
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		registerFilePreviews(this);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
